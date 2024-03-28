f4c3643b4dd426da635b32bc789d2c0a344ff6a5
HirotakaNii <160975417+HirotakaNii@users.noreply.github.com>
2024-03-27 22:20:10 -0300

    Add files via upload

--- imc 2.php
+++ imc 2.php
@@ -11,8 +11,37 @@ return $imc;
 
 }
 
-
 $resp=imc($peso,$alt);
-printf("Olá $nome seu peso é $peso sua altura é $alt seu imc é %.2f" , $resp);
+
+
+
+if($resp<18.5){
+
+echo "  Abaixo do peso "  ;
+
+}
+
+elseif($resp>=18.6 && $resp<=24.9){
+   echo "  Peso ideal, parabéns   <br />"  ;
+}
+
+elseif ($resp>=25.0 && $resp<=29.9) {
+	echo "  Levemente acima do peso <br /> "  ;
+
+}
+elseif ($resp>=30.0 && $resp<=34.9) {
+	echo "  Obesidade grau 1   <br />"  ;
+
+}
+elseif ($resp>=35.0 && $resp<=39.9) {
+	echo "  Obesidade grau 2 (severa)  <br /> "  ;
+
+}
+elseif ($resp>=40.0) {
+	echo "  Obesidade grau 3 (mórbida)  <br />"  ;
+
+}
+
+printf("Olá $nome seu peso é $peso kg sua altura é $alt seu imc é %.2f" , $resp);
 
 ?>
