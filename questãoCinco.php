b56aa3633f267ef90a52a6647c5523c10934541d
HirotakaNii <160975417+HirotakaNii@users.noreply.github.com>
2024-04-10 22:16:29 -0300

    Add files via upload
    att: FINALIZAMOS A QUESTÃO 5

--- questãoCinco.php
+++ questãoCinco.php
@@ -2,17 +2,29 @@
 	$alt1=1.50;
 	$alt2=1.70;
 	$alt3=1.95;
+	
 	$idade1=10;
 	$idade2=20;
 	$idade3=25;
 	$idade4=53;
+	$idade5=92;
+	$idade6=83;
+	
 	$sex1="fem";
 	$sex2="fem";
 	$sex3="fem";
 	$sex4="max";
+	$sex5="max";
+	$sex6="max";
 	$t= 0;
+	$m=0;
 	$cont= 0;
+	$cont2=0;
 	$media= 0;
+	
+	
+
+
 	while ($cont<5){
 
 
@@ -31,36 +43,37 @@
 	}
 
 
+
 	$cont++;
 	}
 
 	echo "$alt3 $alt1";
 
-    if( $sexo = "Fem"){
+    if( $sex = "Fem"){
 
-       $media=10+20+25/3;    
+       $media=($alt1+$alt2+$alt3)/3;    
 
 
 
 	}
 
-      echo("   $media");
+      echo("<br/> media feminin  $media\n");
 
 
 
 
 
-      while ($cont<5){
+      while ($cont<10){
 
 
 
-	if ( $idade1>$idade2){
+	if ( $idade1<$idade2){
 
-	$t = $alt1;
+	$t = $idade1;
 	$idade1=$idade2;
 	$idade2 = $t;
 	}
-	elseif ($idade2>$idade3){
+	elseif ($idade2<$idade3){
 
 	$t = $idade2;
 	$idade2= $idade3;
@@ -68,8 +81,39 @@
 	}
 
 
-	$cont++;
+	$cont++ ;
+	
+    }
+     
+
+
+	echo("$idade3 ");
+ 	
+    
+  
+   while ($cont2<10){
+
+
+
+	if ( $idade4>$idade5){
+
+	$m = $idade4;
+	$idade4=$idade5;
+	$idade5 = $m;
+	}
+	elseif ($idade5>$idade6){
+
+	$m = $idade5;
+	$idade5= $idade6;
+	$idade6 = $m;
 	}
 
-	echo("$idade3 $idade2")
+
+	$cont2++ ;
+	
+    }
+     
+echo("$idade5 $idade6 ");
+
+     
  	?>
