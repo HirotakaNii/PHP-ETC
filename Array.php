bf7fba9f67b1872206ecbbc29ee731ed226fbb48
HirotakaNii <160975417+HirotakaNii@users.noreply.github.com>
2024-04-24 22:16:56 -0300

    Add files via upload

---
+++ Array.php
@@ -0,0 +1,29 @@
+<?php
+
+$name = array("Maria","João","Pedro","Paulo","Lucas</br>");
+
+echo("Os nomes no array:</br>");
+
+foreach ($name as $names){
+	echo"</br>".$names;
+}
+
+
+
+$nome= array("padim","jonin","carlos","clido","sap");
+
+echo("</br>Os nomes no array2B: </br>");
+
+foreach ($nome as $nomes){
+
+echo ("</br>".$nomes. "</br>");
+}
+
+$num = array(1,2,3,4,5);
+
+echo("</br> Os números no array:");
+
+foreach($num as $valor){
+echo "</br>valor  $valor <br/>";
+}
+?>
